2ab8c695094271316ce0db6dee5cc87191c14385
diazturbatig <diazturbati.giulianojeremias@iesconcepcion.edu.ar>
2025-10-28 11:39:54 -0300

    Delete js directory

--- js/productos.js
+++
@@ -1,9 +0,0 @@
-const productos = [
-    { nombre: "Proteína Whey Landerlan Blanca", url: "../Mi primera web/pages/landerlan.blanca.html" },
-    { nombre: "Creatina Landerlan Gold", url: "../Mi primera web/pages/landerlan.gold.html" },
-    { nombre: "BCAA Cooper Pharma", url: "../Mi primera web/pages/cooper.html" },
-    { nombre: "Glutamina UT/LAB", url: "../Mi primera web/pages/utlab.html" },
-    { nombre: "Pre-entreno New Generation", url: "../Mi primera web/pages/new.html" },
-    // Agrega todos tus productos aquí
-  ];
-  
